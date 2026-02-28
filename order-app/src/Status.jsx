14ea9b74ccb89118f219cd7c4139b4ffe0a43d7b
sayyedhakkim2004 <sayyedhakkim2004@gmail.com>
2026-02-28 19:14:18 +0530

    added env url

--- order-app/src/Status.jsx
+++ order-app/src/Status.jsx
@@ -8,7 +8,7 @@ function Manage(){
     const[data,setData]=useState([]);
     
     useEffect(()=>{
-        fetch("http://localhost:3000/users")
+        fetch("`${import.meta.env.VITE_API_URL}/users`")
         .then((response)=> response.json())
         .then((response)=> setData(response))
         .catch((err)=> console.log(err))
