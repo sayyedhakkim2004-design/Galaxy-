7a819891bba40f02cc6f5fdeaef24f922d5f5401
sayyedhakkim2004 <sayyedhakkim2004@gmail.com>
2026-02-28 18:05:13 +0530

    dotenv added

--- index.mjs
+++ index.mjs
@@ -1,13 +1,15 @@
 import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
+import dotenv from "dotenv"
 
 const app=express();
 const PORT=3000;
+dotenv.config()
 
 app.use(express.json());
 app.use(cors());
-mongoose.connect("mongodb://localhost:27017/express")
+mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
     console.log("MongoDb Connected Successfully")
 })
