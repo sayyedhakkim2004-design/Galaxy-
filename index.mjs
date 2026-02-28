463c68098b9c7aa784f8f108347cdd76b272bcd5
sayyedhakkim2004 <sayyedhakkim2004@gmail.com>
2026-02-28 20:39:17 +0530

    modi

--- index.mjs
+++ index.mjs
@@ -7,9 +7,15 @@ dotenv.config();
 const app=express();
 const PORT=process.env.PORT || 3000;
 app.use(express.json());
+
 app.use(cors({
-    origin: "*"
+  origin: "https://marzook.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
+
+app.options("*", cors());
 
 
 const mongoURI = process.env.MONGO_PUBLIC_URL;
