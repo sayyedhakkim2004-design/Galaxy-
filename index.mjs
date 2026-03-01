d55a8c8bace5d86201444d7eed15094228cff6cc
sayyedhakkim2004 <sayyedhakkim2004@gmail.com>
2026-03-01 12:59:38 +0530

    changed /

--- index.mjs
+++ index.mjs
@@ -9,7 +9,7 @@ const PORT=process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://hakkim.vercel.app/",
+  origin: "https://hakkim.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
